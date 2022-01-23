dee6e9bdd388c188dc42a406be3857a4e04a8335
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-23 21:34:03 +0530

    feat(server): use multi part middleware

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import getVideos from './modules/getVideos.js';
 import Server from './server.js';
+import multer from 'multer';
 
-
+const upload = multer();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const apiKey = process.env.KEY;
 
 getVideos(apiKey)
 
-
+app.use(upload.none())
 app.use(cors());
 
 new Server(app, PORT, true);
